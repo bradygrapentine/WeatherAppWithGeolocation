f9a809c0bb0d86b596692b773da524e8a43c7cf9
bradygrapentine <brady.grapentine@gmail.com>
2021-07-23 01:45:04 -0400

    created project, transferred code from front end demo project, and did some basic styling

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+// import React, { useState, useEffect } from 'react'
+import { Route, Switch } from 'react-router-dom'
+// import axios from 'axios'
+import { UserPage } from './pages/UserPage.jsx'
 import './custom.scss'
 
 export function App() {
   return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          {/* <PetList /> */}
+        </Route>
+        <Route exact path="/UserDemo">
+          <UserPage />
+        </Route>
+        {/* <Route exact path="/:id">
+          {/* <PetPage />
+        </Route> */}
+        <Route path="*">Not Found...</Route>
+      </Switch>
+    </>
   )
 }
